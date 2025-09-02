aa41b1d6cba97a829d59eceffee890c261fbd23e
shaswatHota <shaswathota04@gmail.com>
2025-09-02 15:29:39 +0000

    .env fix for firebase private key

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -12,7 +12,7 @@ if (process.env.FIREBASE_SERVICE_ACCOUNT_BASE64) {
         type: "service_account",
         project_id: process.env.FIREBASE_PROJECT_ID,
         private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-        private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'), // Handle escaped newlines
+        private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\\\n/g, '\n').replace(/\\n/g, '\n'), // Handle double-escaped and single-escaped newlines
         client_email: process.env.FIREBASE_CLIENT_EMAIL,
         client_id: process.env.FIREBASE_CLIENT_ID,
         auth_uri: process.env.FIREBASE_AUTH_URI,
